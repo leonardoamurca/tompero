ee5a9ff4f5d2a193fe67cf87e100464a14773999
Leonardo Murça <leonardoamurca@gmail.com>
2021-05-02 11:26:57 -0300

    Make logo clickable

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,13 @@ import styles from "./Header.module.css";
 const Header = ({ children }) => {
   return (
     <div className={styles.Container}>
-      <Logo type="horizontal" />
-      <div className={styles.ButtonGroup}>{children}</div>
+      <div
+        className={styles.LogoContainer}
+        onClick={() => console.log("Go to home page")}
+      >
+        <Logo type="horizontal" />
+      </div>
+      <div>{children}</div>
     </div>
   );
 };
