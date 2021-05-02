efa42784f812b0dc7b2199288ed3eabc8452f55c
Leonardo Murça <leonardoamurca@gmail.com>
2021-05-02 15:46:43 -0300

    Add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
-import styles from './Home.module.css'
+import React, { useState } from "react";
+import styles from "./Home.module.css";
+import { recipes } from "../mocks/recipes";
+import RecipeCard from "../components/RecipeCard";
 
 function Home() {
-
   return (
-   <div className={styles.Container}>
-   		<h1>Screen Home</h1>
-   </div>
+    <div className={styles.Container}>
+      <h1>Receitas Recentes</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          paddingLeft: "100px",
+          paddingRight: "100px",
+        }}
+      >
+        {recipes.map((recipe) => (
+          <RecipeCard
+            title={recipe.title}
+            description={recipe.description}
+            picture={recipe.picture}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
